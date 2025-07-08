32f627197f713878a59f1b439c09fe192750efbf
W D B Sudarshana <115943982+BSudarshana@users.noreply.github.com>
2025-07-09 00:12:20 +0530

    customer controller added
    customer controller has been added

---
+++ server/src/main/java/lk/earth/earthuniversity/dao/AppointmentserviceDao.java
@@ -0,0 +1,9 @@
+package lk.earth.earthuniversity.dao;
+
+import lk.earth.earthuniversity.entity.Appointmentstatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentserviceDao extends JpaRepository<Appointmentstatus,Integer> {
+
+}
+
---
+++ server/src/main/java/lk/earth/earthuniversity/dao/AppointmentstatusDao.java
@@ -0,0 +1,9 @@
+package lk.earth.earthuniversity.dao;
+
+import lk.earth.earthuniversity.entity.Customerstatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentstatusDao extends JpaRepository<Customerstatus,Integer> {
+
+}
+
